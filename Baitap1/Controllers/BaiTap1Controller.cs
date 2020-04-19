62162699c38d92c5aa41ac1c2f5b8983d8145839
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-19 10:35:35 +0700

    Add project files.

---
+++ Baitap1/Controllers/BaiTap1Controller.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Baitap1.Models;
+
+namespace Baitap1.Controllers
+{
+    public class BaiTap1Controller : Controller
+    {
+        // GET: BaiTap1
+        public ActionResult UseRequest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UseRequest(string pt)
+        {
+            double a = double.Parse(Request["a"]);//Chuyển đổi chuỗi sang số thực
+            double b = double.Parse(Request["b"]);
+            pt = Request["pt"].ToString();
+            switch (pt)
+            {
+                case "+": ViewBag.KQ = a + b; break;
+                case "-": ViewBag.KQ = a - b; break;
+                case "*": ViewBag.KQ = a * b; break;
+                case "/":
+                    if (b == 0) ViewBag.KQ = "Không chia được cho 0";
+                    else ViewBag.KQ = a / b; break;
+            }
+            return View();
+        }
+        public ActionResult UseArguments()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UseArguments(string pt)
+        {
+            double a = double.Parse(Request["a"]);
+            double b = double.Parse(Request["b"]);
+            pt = Request["pt"].ToString();
+            switch (pt)
+            {
+                case "+": ViewBag.KQ = a + b; break;
+                case "-": ViewBag.KQ = a - b; break;
+                case "*": ViewBag.KQ = a * b; break;
+                case "/":
+                    if (b == 0) ViewBag.KQ = "Không chia được cho 0";
+                    else ViewBag.KQ = a / b; break;
+            }
+            return View();
+        }
+
+        public ActionResult UseFormCollection()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UseFormCollection(FormCollection f)
+        {
+            double a = double.Parse(f["a"]);//Chuyển đổi chuỗi sang số thực
+            double b = double.Parse(f["b"]);
+            string pt = f["pt"].ToString();
+            switch (pt)
+            {
+                case "+": ViewBag.KQ = a + b; break;
+                case "-": ViewBag.KQ = a - b; break;
+                case "*": ViewBag.KQ = a * b; break;
+                case "/":
+                    if (b == 0) ViewBag.KQ = "Không chia được cho 0";
+                    else ViewBag.KQ = a / b; break;
+            }
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(CalModels cal)
+        {
+            switch (cal.pt)
+            {
+                case "+": ViewBag.KQ = cal.a + cal.b; break;
+                case "-":
+                    ViewBag.KQ = cal.a - cal.b; break;
+                    
+            case "*": ViewBag.KQ = cal.a * cal.b; break;
+                case "/":
+                    if (cal.b == 0) ViewBag.KQ = "Không chia được cho 0";
+                    else ViewBag.KQ = cal.a / cal.b; break;
+            }
+            return View();
+        }
+    }
+}
---
+++ Baitap1/Models/CalModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Baitap1.Models
+{
+    public class CalModels
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public string pt { get; set; }
+    }
+
+}
